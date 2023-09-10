c0d3513ffd210820d687de4d046573edb34f2a87
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2023-09-10 19:06:21 +0530

    commit

---
+++ my-app/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Butcherman } from 'next/font/google'
+
+export default function Home() {
+  return (
+ <div>Hello Admin Dashboard
+    <Button>
+        Click Me
+    </Button>
+ </div>
+  )
+}
